ea442e8d5f8fbfb3124f0aceb7da889b7566191f
Ye Wang <wangyeye.2008@gmail.com>
2017-08-15 15:29:30 +0800

    add Synchronized lyric and Unsynchronized lyric

--- ncm/file_util.py
+++ ncm/file_util.py
@@ -2,7 +2,7 @@
 
 from mutagen.mp3 import MP3, HeaderNotFoundError
 from mutagen.id3 import ID3, APIC, TPE1, TIT2, TALB, TCON, USLT, SYLT, error
-
+import time,datetime,re
 
 def add_metadata_to_song(file_path, cover_path, song, gener, lyric):
     # If no ID3 tags in mp3 file
@@ -51,13 +51,40 @@ def add_metadata_to_song(file_path, cover_path, song, gener, lyric):
             text=gener
         )
     )
-    # add lyric
+    # add Unsychronised lyric
     id3.add(
         USLT(
             encoding=3,
-            lang='XXX',
-            desc=lyric,
+            lang='chs',
+            desc="Unsychronised lyric",
             text=lyric
+        )
+    )
+    # add Synchronized lyric
+    sync_lyric = []
+    for line in lyric.split("\n"):
+        #print(line)
+        if line.find("]") != -1:
+            time_str = line.split("]")[0].replace("[","")
+            lyric_str = line.split("]")[1]
+            try:
+                time_array = re.findall(r"\d+:\d+\.\d+", time_str)
+                if len(time_array) == 1:
+                    time_m = int(time_array[0].split(":")[0])
+                    time_s = int(time_array[0].split(":")[1].split(".")[0])
+                    time_ms = int(time_array[0].split(":")[1].split(".")[1])
+                    sync_lyric.append((lyric_str,time_m*60*1000+time_s*1000+time_ms))
+            except:
+                print(time_str)
+
+    id3.add(
+        SYLT(
+            encoding=3,
+            lang='chs',
+            format=2,
+            type=1,
+            desc="Synchronized lyric",
+            text=sync_lyric
         )
     )
     # add song name
